b122eaf1038ba43694d9a64462ac91eae2285246
Mateus Ferrari <mateusfaa@hotmail.com>
2024-09-24 21:20:38 -0300

    feat: adiciona classe Main com lógica de entrada e verificação de equilíbrio

---
+++ untitled/src/Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        int numeroPresentes = scanner.nextInt();
+        int[] pesosPresentes = new int[numeroPresentes];
+
+        for (int i = 0; i < numeroPresentes; i++) {
+            pesosPresentes[i] = scanner.nextInt();
+        }
+
+
+        if (podeEquilibrar(pesosPresentes, 0, 0)) {
+            System.out.println("S");
+        } else {
+            System.out.println("N");
+        }
+
+        scanner.close();
+    }
+
+    private static boolean podeEquilibrar(int[] pesos, int ladoA, int ladoB) {
+        if (Math.abs(ladoA - ladoB) > 5) {
+            return false;
+        }
+        if (pesos.length == 0) {
+            return true;
+        }
+
+        int pesoAtual = pesos[0];
+        int[] novosPesos = new int[pesos.length - 1];
+        System.arraycopy(pesos, 1, novosPesos, 0, pesos.length - 1);
+
+        return podeEquilibrar(novosPesos, ladoA + pesoAtual, ladoB) ||
+                podeEquilibrar(novosPesos, ladoA, ladoB + pesoAtual);
+    }
+}
